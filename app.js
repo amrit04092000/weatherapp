bf053f55815c4c9b5bfcbde47aedcb2b8fd22464
amrit04092000 <amrit04092000@gmail.com>
2021-06-15 20:09:33 +0530

    'working'

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-window.addEventListener('load',()=>{
+window.addEventListener("load", () => {
   let longi;
   let lati;
-  let temperatureDiscription = document.querySelector(".temperature-discription");
+  let temperatureDiscription = document.querySelector(
+    ".temperature-discription"
+  );
   let temperatureDegree = document.querySelector(".temperature-degree");
   let locationTimezone = document.querySelector(".location-timezone");
-  let temperatureSection= document.querySelector(".temperature");
-  let temperatureSpan= document.querySelector(".temperature span");
+  let temperatureSection = document.querySelector(".temperature");
+  let temperatureSpan = document.querySelector(".temperature span");
   let cityName = document.querySelector(".search");
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position=>{
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
       longi = position.coords.longitude;
-      lati = position.coords.latitude; 
+      lati = position.coords.latitude;
 
-      const api= `https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${longi}&units=metric&appid=4d9ffb85fec653fe8e30f3a4ed0c5b20`;
-    
-    fetch(api)
-      .then(response =>{
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${longi}&units=metric&appid=4d9ffb85fec653fe8e30f3a4ed0c5b20`;
+
+      fetch(api)
+        .then((response) => {
           return response.json();
-      })
-      .then(data =>{
+        })
+        .then((data) => {
           console.log(data);
-          const {temp}= data.main;
-          const {main} = data.weather[0];
-          const {icon} = data.weather[0];
-          let fahrenhiet = (temp*(9/5))+32;
+          const { temp } = data.main;
+          const { main } = data.weather[0];
+          const { icon } = data.weather[0];
+          let fahrenhiet = temp * (9 / 5) + 32;
           //icon image url
           const iconurl = `http://openweathermap.org/img/w/${icon}.png`;
 
@@ -32,55 +34,21 @@ window.addEventListener('load',()=>{
           temperatureDiscription.textContent = main;
           locationTimezone.textContent = data.name;
           //showing icon image
-          document.getElementById("wicon").src=iconurl;
+          document.getElementById("wicon").src = iconurl;
 
           //change temperature C to F
-          temperatureSection.addEventListener("click",()=>{
-            if(temperatureSpan.textContent === "C"){
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "C") {
               temperatureSpan.textContent = "F";
-              temperatureDegree.textContent = fahrenhiet;
-            }else{
+              temperatureDegree.textContent = fahrenhiet.toFixed(2);
+            } else {
               temperatureSpan.textContent = "C";
               temperatureDegree.textContent = temp;
             }
           });
-      });
-    });
-  }
-
-  function cityTemp(){
-    console.log(cityName);
-    const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4d9ffb85fec653fe8e30f3a4ed0c5b20`;
-    fetch(api)
-    .then(response =>{
-        return response.json();
-    })
-    .then(data =>{
-        console.log(data);
-        const {temp}= data.main;
-        const {main} = data.weather[0];
-        const {icon} = data.weather[0];
-        let fahrenhiet = (temp*(9/5))+32;
-        //icon image url
-        const iconurl = `http://openweathermap.org/img/w/${icon}.png`;
-
-        //Set DOM Elements from the API
-        temperatureDegree.textContent = temp;
-        temperatureDiscription.textContent = main;
-        locationTimezone.textContent = data.name;
-        //showing icon image
-        document.getElementById("wicon").src=iconurl;
-
-        //change temperature C to F
-        temperatureSection.addEventListener("click",()=>{
-          if(temperatureSpan.textContent === "C"){
-            temperatureSpan.textContent = "F";
-            temperatureDegree.textContent = fahrenhiet;
-          }else{
-            temperatureSpan.textContent = "C";
-            temperatureDegree.textContent = temp;
-          }
         });
     });
-}
+  }
 });
+
+
--- searchWeather.js
+++ searchWeather.js
@@ -1,45 +1,44 @@
 
-window.addEventListener('load',()=>{
-    let temperatureDiscription = document.querySelector(".temperature-discription");
-    let temperatureDegree = document.querySelector(".temperature-degree");
-    let locationTimezone = document.querySelector(".location-timezone");
-    let temperatureSection= document.querySelector(".temperature");
-    let temperatureSpan= document.querySelector(".temperature span");
-
-
-    function cityTemp(){
-    let cityName = document.getElementById("search").value;
-    const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4d9ffb85fec653fe8e30f3a4ed0c5b20`;
-    fetch(api)
-    .then(response =>{
-        return response.json();
+function cityTemp() {
+  let temperatureDiscription = document.querySelector(
+    ".temperature-discription"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature");
+  let temperatureSpan = document.querySelector(".temperature span");
+  let cityName = document.getElementById("search").value;
+  console.log(cityName);
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=4d9ffb85fec653fe8e30f3a4ed0c5b20`;
+  fetch(api)
+    .then((response) => {
+      return response.json();
     })
-    .then(data =>{
-        console.log(data);
-        const {temp}= data.main;
-        const {main} = data.weather[0];
-        const {icon} = data.weather[0];
-        let fahrenhiet = (temp*(9/5))+32;
-        //icon image url
-        const iconurl = `http://openweathermap.org/img/w/${icon}.png`;
+    .then((data) => {
+      console.log(data);
+      const { temp } = data.main;
+      const { main } = data.weather[0];
+      const { icon } = data.weather[0];
+      let fahrenhiet = temp * (9 / 5) + 32;
+      //icon image url
+      const iconurl = `http://openweathermap.org/img/w/${icon}.png`;
 
-        //Set DOM Elements from the API
-        temperatureDegree.textContent = temp;
-        temperatureDiscription.textContent = main;
-        locationTimezone.textContent = data.name;
-        //showing icon image
-        document.getElementById("wicon").src=iconurl;
+      //Set DOM Elements from the API
+      temperatureDegree.textContent = temp;
+      temperatureDiscription.textContent = main;
+      locationTimezone.textContent = data.name;
+      //showing icon image
+      document.getElementById("wicon").src = iconurl;
 
-        //change temperature C to F
-        temperatureSection.addEventListener("click",()=>{
-          if(temperatureSpan.textContent === "C"){
-            temperatureSpan.textContent = "F";
-            temperatureDegree.textContent = fahrenhiet;
-          }else{
-            temperatureSpan.textContent = "C";
-            temperatureDegree.textContent = temp;
-          }
-        });
+      //change temperature C to F
+      temperatureSection.addEventListener("click", () => {
+        if (temperatureSpan.textContent === "C") {
+          temperatureSpan.textContent = "F";
+          temperatureDegree.textContent = fahrenhiet.toFixed(2);
+        } else {
+          temperatureSpan.textContent = "C";
+          temperatureDegree.textContent = temp;
+        }
+      });
     });
 }
-})
